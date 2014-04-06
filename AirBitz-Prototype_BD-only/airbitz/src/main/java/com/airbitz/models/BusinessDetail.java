2aae2852fc52c2d7913f309e1da3719e1eeffe3f
Chris Arriola <arriolac279@gmail.com>
2014-04-06 13:53:26 -0700

    Removing comma right after city.
    Former-commit-id: e42d210082a0d791d601a1cae779079f12dfbf38

--- AirBitz-Prototype_BD-only/airbitz/src/main/java/com/airbitz/models/BusinessDetail.java
+++ AirBitz-Prototype_BD-only/airbitz/src/main/java/com/airbitz/models/BusinessDetail.java
@@ -271,7 +271,7 @@ public class BusinessDetail {
         ssb.append(mBizAddress)
                 .append("\n")
                 .append(mBizCity)
-                .append(", ")
+                .append(" ")
                 .append(mBizState)
                 .append(" ")
                 .append(mBizPostalCode);
