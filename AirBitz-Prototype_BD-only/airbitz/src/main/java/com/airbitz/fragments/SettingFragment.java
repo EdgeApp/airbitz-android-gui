206b94ec18bb7905694971dad41906b2c82613f0
Tom Baker <baker.tom.w@gmail.com>
2014-05-01 15:00:08 -0700

    adjust Settings UI

--- AirBitz-Prototype_BD-only/airbitz/src/main/java/com/airbitz/fragments/SettingFragment.java
+++ AirBitz-Prototype_BD-only/airbitz/src/main/java/com/airbitz/fragments/SettingFragment.java
@@ -10,9 +10,14 @@ import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.ToggleButton;
 
 import com.airbitz.R;
 import com.airbitz.activities.DisplayActivity;
@@ -25,18 +30,33 @@ import com.airbitz.utils.Common;
  */
 public class SettingFragment extends Fragment {
 
-    private Button mDisplayButton;
-    private Button mSecurityButton;
-    private Button mLanguageButton;
-    private Button mExchangeRateButton;
-    private Button mCategoriesButton;
-
     private ImageButton mBackButton;
     private ImageButton mHelpButton;
-
     private TextView mTitleTextView;
-    private TextView mLanguageTextView;
-    private TextView mExchangeTextView;
+
+    private Button mBitcoinButton;
+    private Button mmBitcoinButton;
+    private Button muBitcoinButton;
+
+    private Button mChangePasswordButton;
+    private Button mChangePINButton;
+    private Button mChangeRecoveryButton;
+
+    private ToggleButton mSendNameToggle;
+    private EditText mFirstEditText;
+    private EditText mLastEditText;
+    private EditText mNicknameEditText;
+
+    private Spinner mAutoLogoffSpinner;
+    private Spinner mLanguageSpinner;
+    private Spinner mCurrencySpinner;
+
+    private Spinner mUSDollarSpinner;
+    private Spinner mCanadianDollarSpinner;
+    private Spinner mEuroSpinner;
+    private Spinner mPesoSpinner;
+    private Spinner mYuanSpinner;
+
 
     @Override
     public void onCreate(Bundle savedInstanceState)
@@ -48,81 +68,150 @@ public class SettingFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_setting, container, false);
 
+        mBackButton = (ImageButton) view.findViewById(R.id.settings_button_back);
+        mHelpButton = (ImageButton) view.findViewById(R.id.settings_button_help);
+        mTitleTextView = (TextView) view.findViewById(R.id.settings_textview_title);
+        mTitleTextView.setTypeface(LandingActivity.montserratBoldTypeFace);
+
+        mBitcoinButton = (Button) view.findViewById(R.id.settings_denomination_buttons_bitcoin);
+        mmBitcoinButton = (Button) view.findViewById(R.id.settings_denomination_buttons_mbitcoin);
+        muBitcoinButton = (Button) view.findViewById(R.id.settings_denomination_buttons_ubitcoin);
+
+        mChangePasswordButton = (Button) view.findViewById(R.id.settings_button_change_password);
+        mChangePINButton = (Button) view.findViewById(R.id.settings_button_pin);
+        mChangeRecoveryButton = (Button) view.findViewById(R.id.settings_button_recovery);
+
+        mSendNameToggle = (ToggleButton) view.findViewById(R.id.settings_toggle_send_user_info);
+        mFirstEditText = (EditText) view.findViewById(R.id.settings_edit_first_name);
+        mLastEditText = (EditText) view.findViewById(R.id.settings_edit_last_name);
+        mNicknameEditText = (EditText) view.findViewById(R.id.settings_edit_nick_name);
+
+        mAutoLogoffSpinner = (Spinner) view.findViewById(R.id.settings_spinner_auto_logoff);
+        mLanguageSpinner = (Spinner) view.findViewById(R.id.settings_spinner_language);
+        mCurrencySpinner = (Spinner) view.findViewById(R.id.settings_spinner_currency);
+
+        mUSDollarSpinner = (Spinner) view.findViewById(R.id.settings_spinner_usd);
+        mCanadianDollarSpinner = (Spinner) view.findViewById(R.id.settings_spinner_canadian);
+        mEuroSpinner = (Spinner) view.findViewById(R.id.settings_spinner_euro);
+        mPesoSpinner = (Spinner) view.findViewById(R.id.settings_spinner_peso);
+        mYuanSpinner = (Spinner) view.findViewById(R.id.settings_spinner_yuan);
+
 //        mDisplayButton = (Button) view.findViewById(R.id.button_display);
-//        mSecurityButton = (Button) view.findViewById(R.id.button_security);
-//        mLanguageButton = (Button) view.findViewById(R.id.button_language);
-//        mExchangeRateButton = (Button) view.findViewById(R.id.button_exchange);
-//        mCategoriesButton = (Button) view.findViewById(R.id.button_categories);
-//
-//        mLanguageTextView = (TextView) view.findViewById(R.id.language_chosen);
-//        mExchangeTextView = (TextView) view.findViewById(R.id.exchange_rate_chosen);
-//
-//        mBackButton = (ImageButton) view.findViewById(R.id.button_back);
-//        mHelpButton = (ImageButton) view.findViewById(R.id.button_help);
-//
-//        mTitleTextView = (TextView) view.findViewById(R.id.textview_title);
-//
-//        mTitleTextView.setTypeface(LandingActivity.montserratBoldTypeFace);
-//        mDisplayButton.setTypeface(LandingActivity.latoBlackTypeFace);
-//        mSecurityButton.setTypeface(LandingActivity.latoBlackTypeFace);
-//        mLanguageButton.setTypeface(LandingActivity.latoBlackTypeFace);
-//        mExchangeRateButton.setTypeface(LandingActivity.latoBlackTypeFace);
 //        mCategoriesButton.setTypeface(LandingActivity.latoBlackTypeFace);
 //
 //        mLanguageTextView.setTypeface(LandingActivity.latoBlackTypeFace, Typeface.ITALIC);
-//        mExchangeTextView.setTypeface(LandingActivity.latoBlackTypeFace, Typeface.ITALIC);
 //
-//        mBackButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-////                finish();
-//            }
-//        });
-//        mHelpButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Common.showHelpInfo(getActivity(), "Info", "Business directory info");
-//            }
-//        });
-//
-//        mDisplayButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Intent intent = new Intent(getActivity(), DisplayActivity.class);
-//                startActivity(intent);
-//            }
-//        });
-//
-//        mSecurityButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Intent intent = new Intent(getActivity(), SecurityActivity.class);
-//                startActivity(intent);
-//            }
-//        });
-//
-//        mLanguageButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//
-//            }
-//        });
-//
-//        mExchangeRateButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//
-//            }
-//        });
-//
-//        mCategoriesButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//
-//            }
-//        });
+        mBackButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                finish();
+            }
+        });
+        mHelpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Common.showHelpInfo(getActivity(), "Info", "Business directory info");
+            }
+        });
+
+        mBitcoinButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            }
+        });
+
+        mmBitcoinButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            }
+        });
+
+        muBitcoinButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            }
+        });
+
+        mChangePasswordButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            }
+        });
+
+        mChangePINButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            }
+        });
+
+        mChangeRecoveryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            }
+        });
+
+        mSendNameToggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                // Save the state here
+                setUserNameState(isChecked);
+            }
+        });
+
+        //TODO setup spinners
+        String[] auto_logoff_items = getResources().getStringArray(R.array.auto_logoff_array);
+        ArrayAdapter<String> auto_logoff_adapter = new ArrayAdapter<String>(getActivity(),  R.layout.item_setting_spinner, auto_logoff_items);
+        mAutoLogoffSpinner.setAdapter(auto_logoff_adapter);
+
+        String[] language_items = getResources().getStringArray(R.array.language_array);
+        ArrayAdapter<String> language_adapter = new ArrayAdapter<String>(getActivity(),  R.layout.item_setting_spinner, language_items);
+        mLanguageSpinner.setAdapter(language_adapter);
+
+        String[] currency_items = getResources().getStringArray(R.array.default_currency_array);
+        ArrayAdapter<String> currency_adapter = new ArrayAdapter<String>(getActivity(),  R.layout.item_setting_spinner, currency_items);
+        mCurrencySpinner.setAdapter(currency_adapter);
+
+        String[] usd_items = getResources().getStringArray(R.array.usd_exchange_array);
+        ArrayAdapter<String> usd_adapter = new ArrayAdapter<String>(getActivity(),  R.layout.item_setting_spinner, usd_items);
+        mUSDollarSpinner.setAdapter(usd_adapter);
+
+        String[] canadian_items = getResources().getStringArray(R.array.canadian_exchange_array);
+        ArrayAdapter<String> canadian_adapter = new ArrayAdapter<String>(getActivity(),  R.layout.item_setting_spinner, canadian_items);
+        mCanadianDollarSpinner.setAdapter(canadian_adapter);
+
+        String[] euro_items = getResources().getStringArray(R.array.euro_exchange_array);
+        ArrayAdapter<String> euro_adapter = new ArrayAdapter<String>(getActivity(),  R.layout.item_setting_spinner, euro_items);
+        mEuroSpinner.setAdapter(euro_adapter);
+
+        String[] peso_items = getResources().getStringArray(R.array.peso_exchange_array);
+        ArrayAdapter<String> peso_adapter = new ArrayAdapter<String>(getActivity(),  R.layout.item_setting_spinner, peso_items);
+        mPesoSpinner.setAdapter(peso_adapter);
+
+        String[] yuan_items = getResources().getStringArray(R.array.yuan_exchange_array);
+        ArrayAdapter<String> yuan_adapter = new ArrayAdapter<String>(getActivity(),  R.layout.item_setting_spinner, yuan_items);
+        mYuanSpinner.setAdapter(yuan_adapter);
+
+        //TODO populate from PREFS
 
         return view;
+    }
+
+    private void setUserNameState(boolean on) {
+        if(on) {
+            mFirstEditText.setEnabled(true);
+            mFirstEditText.setBackgroundResource(R.drawable.rounded_edge_black_transparent_30_padded);
+            mLastEditText.setEnabled(true);
+            mLastEditText.setBackgroundResource(R.drawable.rounded_edge_black_transparent_30_padded);
+            mNicknameEditText.setEnabled(true);
+            mNicknameEditText.setBackgroundResource(R.drawable.rounded_edge_black_transparent_30_padded);
+        } else {
+            mFirstEditText.setEnabled(false);
+            mFirstEditText.setBackgroundResource(R.drawable.rounded_edge_black_transparent);
+            mLastEditText.setEnabled(false);
+            mLastEditText.setBackgroundResource(R.drawable.rounded_edge_black_transparent);
+            mNicknameEditText.setEnabled(false);
+            mNicknameEditText.setBackgroundResource(R.drawable.rounded_edge_black_transparent);
+        }
     }
 
     @Override
@@ -134,7 +223,6 @@ public class SettingFragment extends Fragment {
     @Override
     public void onPause() {
         super.onPause();
-        //TODO save state here
     }
 
 }
