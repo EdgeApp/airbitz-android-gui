ae7cb6e237fd1ea73cc3402457b5874999d655a9
Tom Baker <baker.tom.w@gmail.com>
2014-05-26 11:52:33 -0700

    remerge
    Former-commit-id: 2c6797a3b136c89f2caa1a388a1320abb33c2243

--- AirBitz-Prototype_BD-only/airbitz/src/main/jni/Android.mk
+++ AirBitz-Prototype_BD-only/airbitz/src/main/jni/Android.mk
@@ -2,8 +2,9 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := airbitz
-LOCAL_SRC_FILES := ABCjni.cpp
-#LOCAL_SHARED_LIBRARIES := libstuff.so
-LOCAL_LDFLAGS := -lstuff -L.
+LOCAL_SRC_FILES := ABC_wrap.c
+#LOCAL_SHARED_LIBRARIES := libabc.so
+LOCAL_LDFLAGS := -labc -L.
+LOCAL_LDLIBS  := -llog
 
 include $(BUILD_SHARED_LIBRARY)
