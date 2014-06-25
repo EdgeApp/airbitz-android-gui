5d4e122b3df05c44651720c7635a917a10079288
Matt Stansbury <mstansbu@gmail.com>
2014-06-25 12:39:47 -0700

    continued work on category
    Former-commit-id: 0155c7a47ea920527833042ca5f2831a8d8a4eb5

--- AirBitz-Prototype_BD-only/airbitz/src/main/java/com/airbitz/fragments/CategoryFragment.java
+++ AirBitz-Prototype_BD-only/airbitz/src/main/java/com/airbitz/fragments/CategoryFragment.java
@@ -8,6 +8,7 @@ import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
 import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
@@ -16,6 +17,7 @@ import android.widget.TextView;
 
 import com.airbitz.R;
 import com.airbitz.adapters.SettingsCategoryAdapter;
+import com.airbitz.models.CategoryTypeEnum;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -31,6 +33,8 @@ public class CategoryFragment extends Fragment {
     private Button mAddButton;
     private Button mCancelButton;
     private Button mDoneButton;
+
+    private CategoryTypeEnum currentType = CategoryTypeEnum.Expense;
 
     private ListView mCategoryListView;
     private SettingsCategoryAdapter mCategoryAdapter;
@@ -55,11 +59,14 @@ public class CategoryFragment extends Fragment {
         mCancelButton = (Button) view.findViewById(R.id.button_cancel);
         mDoneButton = (Button) view.findViewById(R.id.button_done);
 
+
+
         mCategoryListView = (ListView) view.findViewById(R.id.category_list_view);
         mCategories = new ArrayList<String>();
         mCurrentCategories = new ArrayList<String>();
         goAddCategories();
         mCategoryAdapter = new SettingsCategoryAdapter(getActivity(),mCurrentCategories);
+        mCategoryListView.setAdapter(mCategoryAdapter);
 
         //  EditTexts
         mSearchField.addTextChangedListener(new TextWatcher() {
@@ -75,13 +82,22 @@ public class CategoryFragment extends Fragment {
 
             @Override
             public void afterTextChanged(Editable editable) {
-
+                mCurrentCategories.clear();
+                for(String s :mCategories){
+                    if(s.toLowerCase().contains(editable.toString().toLowerCase())){
+                        mCurrentCategories.add(s);
+                    }
+                }
+                mCategoryAdapter.notifyDataSetChanged();
             }
         });
 
         mAddField.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
-            public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
+            public boolean onEditorAction(TextView textView, int actionId, KeyEvent keyEvent) {
+                if(actionId == EditorInfo.IME_ACTION_DONE){
+                    goAddNewCategory();
+                }
                 return false;
             }
         });
@@ -91,29 +107,23 @@ public class CategoryFragment extends Fragment {
         mAddButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                goAddNewCategory();
             }
         });
 
         mCancelButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                //TODO Popup
+                getActivity().onBackPressed();
             }
         });
 
         mDoneButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-            }
-        });
-
-        //  ListView
-        mCategoryListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-
+                //TODO Save to core
+                getActivity().onBackPressed();
             }
         });
 
@@ -126,5 +136,16 @@ public class CategoryFragment extends Fragment {
         mCategories.add("Expense:Cotton Candy");
         mCategories.add("Income:Newspaper Route");
         mCurrentCategories.addAll(mCategories);
+    }
+
+    private void goAddNewCategory(){
+        String newCat;
+        if(currentType == CategoryTypeEnum.Income){
+            newCat = mAddField.getText().toString().substring(7);
+        }else if(currentType == CategoryTypeEnum.Expense){
+
+        }else if(currentType == CategoryTypeEnum.Transfer){
+
+        }
     }
 }
