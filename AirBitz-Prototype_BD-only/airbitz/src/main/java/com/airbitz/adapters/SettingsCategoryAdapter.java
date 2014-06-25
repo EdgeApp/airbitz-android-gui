0155c7a47ea920527833042ca5f2831a8d8a4eb5
Matt Stansbury <mstansbu@gmail.com>
2014-06-25 12:39:47 -0700

    continued work on category

--- AirBitz-Prototype_BD-only/airbitz/src/main/java/com/airbitz/adapters/SettingsCategoryAdapter.java
+++ AirBitz-Prototype_BD-only/airbitz/src/main/java/com/airbitz/adapters/SettingsCategoryAdapter.java
@@ -1,10 +1,14 @@
 package com.airbitz.adapters;
 
 import android.content.Context;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.airbitz.R;
 
@@ -28,6 +32,44 @@ public class SettingsCategoryAdapter extends ArrayAdapter<String> {
     public View getView(int position, View convertView, ViewGroup parent) {
         LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         convertView = inflater.inflate(R.layout.item_listview_settings_categories, parent, false);
+
+        EditText mCategoryName = (EditText) convertView.findViewById(R.id.category_field);
+        mCategoryName.setText(mCategories.get(position));
+
+        mCategoryName.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean hasFocus) {
+
+            }
+        });
+
+        mCategoryName.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i2, int i3) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i2, int i3) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+        final int pos = position;
+        Button mDeleteButton = (Button) convertView.findViewById(R.id.category_delete);
+        mDeleteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mCategories.remove(pos);
+                //TODO Remove From Core
+                notifyDataSetChanged();
+            }
+        });
 
         return convertView;
     }
