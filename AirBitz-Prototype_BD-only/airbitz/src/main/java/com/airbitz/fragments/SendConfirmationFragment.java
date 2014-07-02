a100799327655d8ac60dd538c4fd445cd645ed96
Tom Baker <baker.tom.w@gmail.com>
2014-07-02 12:15:22 -0700

    unfinished core integration - still goes directly to receive success
    Former-commit-id: e9605a1fddd3cf09ff2e86286e221b95823f0a0a

--- AirBitz-Prototype_BD-only/airbitz/src/main/java/com/airbitz/fragments/SendConfirmationFragment.java
+++ AirBitz-Prototype_BD-only/airbitz/src/main/java/com/airbitz/fragments/SendConfirmationFragment.java
@@ -19,14 +19,23 @@ import android.widget.RelativeLayout;
 import android.widget.ScrollView;
 import android.widget.TextView;
 
+import com.airbitz.AirbitzApplication;
 import com.airbitz.R;
 import com.airbitz.activities.NavigationActivity;
+import com.airbitz.api.CoreAPI;
+import com.airbitz.api.SWIGTYPE_p_int64_t;
+import com.airbitz.api.SWIGTYPE_p_uint64_t;
+import com.airbitz.api.core;
+import com.airbitz.api.tABC_CC;
+import com.airbitz.api.tABC_Error;
+import com.airbitz.api.tABC_TxDetails;
+import com.airbitz.api.tABC_WalletInfo;
 import com.airbitz.utils.Common;
 
 /**
  * Created on 2/21/14.
  */
-public class SendConfirmationFragment extends Fragment{
+public class SendConfirmationFragment extends Fragment {
 
     private TextView mFromEdittext;
     private TextView mToEdittext;
@@ -73,16 +82,18 @@ public class SendConfirmationFragment extends Fragment{
     private boolean mSuccess = false;
 
     private String mQRResult;
+    private CoreAPI mCoreAPI;
 
     @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         bundle = this.getArguments();
-        if(bundle == null){
+        if (bundle == null) {
             System.out.println("Send confirmation bundle is null");
+        } else {
+            mQRResult = bundle.getString(SendFragment.QR_RESULT);
         }
-        mQRResult = bundle.getString(SendFragment.QR_RESULT);
+        mCoreAPI = CoreAPI.getApi();
     }
 
     @Override
@@ -131,21 +142,21 @@ public class SendConfirmationFragment extends Fragment{
         mParentLayout = (RelativeLayout) view.findViewById(R.id.layout_root);
         mScrollView = (ScrollView) view.findViewById(R.id.layout_scroll);
 
-        mConfirmCenter = mConfirmSwipeButton.getWidth()/2;
+        mConfirmCenter = mConfirmSwipeButton.getWidth() / 2;
 
         mFromEdittext.setText(bundle.getString("wallet_name"));
         mToEdittext.setText(bundle.getString("to_name"));
 
 
-        Shader textShader=new LinearGradient(0, 0, 0, 20,
-                new int[]{Color.parseColor("#ffffff"),Color.parseColor("#addff1")},
+        Shader textShader = new LinearGradient(0, 0, 0, 20,
+                new int[]{Color.parseColor("#ffffff"), Color.parseColor("#addff1")},
                 new float[]{0, 1}, Shader.TileMode.CLAMP);
         mSlideTextView.getPaint().setShader(textShader);
 
         mConfirmImageView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(mConfirmChecked){
+                if (mConfirmChecked) {
                     mConfirmImageView.setImageResource(R.drawable.btn_confirm_off);
                     mConfirmChecked = false;
                 } else {
@@ -173,7 +184,7 @@ public class SendConfirmationFragment extends Fragment{
         mSlideLayout.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
             @Override
             public void onGlobalLayout() {
-                if(!mSuccess){
+                if (!mSuccess) {
                     mLeftThreshold = (int) mSlideLayout.getX();
                 }
             }
@@ -182,7 +193,7 @@ public class SendConfirmationFragment extends Fragment{
         mConfirmSwipeButton.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
             @Override
             public void onGlobalLayout() {
-                if(!mSuccess){
+                if (!mSuccess) {
                     mRightThreshold = (int) mConfirmSwipeButton.getX();
                 }
             }
@@ -199,7 +210,7 @@ public class SendConfirmationFragment extends Fragment{
                     case MotionEvent.ACTION_MOVE:
                         rX = event.getRawX();
                         float delta = rX - dX;
-                        if (delta < mLeftThreshold-mConfirmCenter) {
+                        if (delta < mLeftThreshold - mConfirmCenter) {
                             mConfirmSwipeButton.setX(mLeftThreshold - mConfirmCenter);
                         } else if (delta > mRightThreshold) {
                             mConfirmSwipeButton.setX(mRightThreshold);
@@ -265,15 +276,133 @@ public class SendConfirmationFragment extends Fragment{
         return view;
     }
 
-    public void touchEventsEnded(){
-        int successThreshold = mLeftThreshold + (mSlideLayout.getWidth()/4);
-        boolean pinGood = true;//TODO check pin number, and pop up if not
-        if(mConfirmSwipeButton.getX() <= successThreshold && pinGood){
+    public void touchEventsEnded() {
+        int successThreshold = mLeftThreshold + (mSlideLayout.getWidth() / 4);
+        if (mConfirmSwipeButton.getX() <= successThreshold) {
+//            attemptInitiateSend();
+
             Fragment frag = new ReceivedSuccessFragment();
             frag.setArguments(bundle);
             ((NavigationActivity) getActivity()).pushFragment(frag);
-        }else{
+        } else {
             mConfirmSwipeButton.setX(mRightThreshold);
         }
+    }
+
+    private void attemptInitiateSend() {
+        //make sure PIN is good
+        String enteredPIN = "";
+        if (!enteredPIN.isEmpty()) {
+            //make sure the entered PIN matches the PIN stored in the Core
+            tABC_Error error = new tABC_Error();
+            String szPIN = null;
+
+//            mCoreAPI.ABC_GetPIN(AirbitzApplication.getUsername(), AirbitzApplication.getPassword(),
+//                szPIN, error);
+
+            if (szPIN!=null && szPIN.equals(enteredPIN)) {
+                    InitiateSendRequest();
+                    return;
+            }
+//            free(szPIN);
+        }
+        showIncorrectPINAlert();
+//        [_confirmationSlider resetIn:1.0];
+    }
+
+    private void InitiateSendRequest() {
+//            tABC_Error Error = new tABC_Error();
+//            tABC_CC result;
+//            int nCount;
+//            double currency;
+//
+//        long satoshi = 1234; //TODO
+//        int currencyNum = 840; //TODO
+//            currency = mCoreAPI.SatoshiToCurrency(satoshi, currencyNum);
+//            if (result == ABC_CC_Ok)
+//            {
+//                ABC_GetWallets([[User Singleton].name UTF8String], [[User Singleton].password UTF8String], &aWalletInfo, &nCount, &Error);
+//
+//                if (nCount)
+//                {
+//                    tABC_TxDetails Details;
+//
+//                    SWIGTYPE_p_int64_t pAmountSatoshi = core.longp_to
+//                    Details.setAmountSatoshi(mBitcoinValueField.getText().toString());
+//                    Details.amountCurrency = currency;
+//                    // These will be calculated for us
+//                    Details.amountFeesAirbitzSatoshi = 0;
+//                    Details.amountFeesMinersSatoshi = 0;
+//                    // If this is a transfer, populate the comments
+//                    Details.szName = "Anonymous";
+//                    Details.szCategory = "";
+//                    Details.szNotes = "";
+//                    Details.attributes = 0x2;
+//
+//                    tABC_WalletInfo *info = aWalletInfo[self.selectedWalletIndex];
+//
+//                    if (self.bAddressIsWalletUUID)
+//                    {
+//                        NSString *categoryText = NSLocalizedString(@"Transfer:Wallet:", nil);
+//                        tABC_TransferDetails Transfer;
+//                        Transfer.szSrcWalletUUID = strdup(info->szUUID);
+//                        Transfer.szSrcName = strdup([self.destWallet.strName UTF8String]);
+//                        Transfer.szSrcCategory = strdup([[NSString stringWithFormat:@"%@%@", categoryText, self.destWallet.strName] UTF8String]);
+//
+//                        Transfer.szDestWalletUUID = strdup([self.destWallet.strUUID UTF8String]);
+//                        Transfer.szDestName = strdup([self.wallet.strName UTF8String]);
+//                        Transfer.szDestCategory = strdup([[NSString stringWithFormat:@"%@%@", categoryText, self.wallet.strName] UTF8String]);
+//
+//                        result = ABC_InitiateTransfer([[User Singleton].name UTF8String],
+//                        [[User Singleton].password UTF8String],
+//                        &Transfer, &Details,
+//                            ABC_SendConfirmation_Callback,
+//                            (__bridge void *)self,
+//                        &Error);
+//
+//                        free(Transfer.szSrcWalletUUID);
+//                        free(Transfer.szSrcName);
+//                        free(Transfer.szSrcCategory);
+//                        free(Transfer.szDestWalletUUID);
+//                        free(Transfer.szDestName);
+//                        free(Transfer.szDestCategory);
+//                    } else {
+//                        result = ABC_InitiateSendRequest([[User Singleton].name UTF8String],
+//                        [[User Singleton].password UTF8String],
+//                        info->szUUID,
+//                        [self.sendToAddress UTF8String],
+//                        &Details,
+//                                ABC_SendConfirmation_Callback,
+//                                (__bridge void *)self,
+//                        &Error);
+//                    }
+//                    if (result == ABC_CC_Ok)
+//                    {
+//                        [self showSendStatus];
+//                    }
+//                    else
+//                    {
+//                        [Util printABC_Error:&Error];
+//                    }
+//
+////                    ABC_FreeWalletInfoArray(aWalletInfo, nCount);
+//                }
+//            }
+        }
+
+    private void showIncorrectPINAlert() {
+//                    UIAlertView * alert =[[UIAlertView alloc]
+//                    initWithTitle:
+//                    NSLocalizedString( @ "Incorrect PIN", nil)
+//                    message:
+//                    NSLocalizedString( @
+//                    "You must enter the correct withdrawl PIN in order to proceed", nil)
+//                    delegate:
+//                    self
+//                    cancelButtonTitle:
+//                    @ "OK"
+//                    otherButtonTitles:
+//                    nil];
+//                    [alert show];
     }
 }
