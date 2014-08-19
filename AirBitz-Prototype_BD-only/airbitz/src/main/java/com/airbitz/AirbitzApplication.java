8f8155786fc75c445cacbb374d34fea65af3cce7
Thomas Baker <baker.tom.w@gmail.com>
2014-08-19 13:23:52 -0700

    autologin change
    Former-commit-id: 16d10f11ad58703b1622c3033ffc0a10276d97db

--- AirBitz-Prototype_BD-only/airbitz/src/main/java/com/airbitz/AirbitzApplication.java
+++ AirBitz-Prototype_BD-only/airbitz/src/main/java/com/airbitz/AirbitzApplication.java
@@ -13,8 +13,7 @@ public class AirbitzApplication extends Application {
     public static final boolean DEBUG_LOGGING = true;
 
     public static boolean AUTOLOGIN = false;
-    private static String loginName = "testjbvffvj"; private static String loginPassword = "Aaaaaaaa1@";
-//    private static String loginName = "matt104"; private static String loginPassword = "O@21grapes";
+    private static String loginName = "tbtest"; private static String loginPassword = "Aaaaaaaa1@";
 
     public static String PREFS = "com.airbitz.prefs";
     public static String LOGIN_NAME = "com.airbitz.login_name";
