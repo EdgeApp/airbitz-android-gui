3240d153e53f0326ae8a63d3040e362ebc70dff5
Tom Baker <baker.tom.w@gmail.com>
2014-08-26 16:04:41 -0700

    revert last change

--- AirBitz-Prototype_BD-only/airbitz/src/main/java/com/airbitz/activities/NavigationActivity.java
+++ AirBitz-Prototype_BD-only/airbitz/src/main/java/com/airbitz/activities/NavigationActivity.java
@@ -244,7 +244,7 @@ public class NavigationActivity extends BaseActivity
     }
 
     public void switchFragmentThread(int id) {
-        if(!bdonly) {
+        if(mNavBarFragmentLayout.getVisibility() != View.VISIBLE) {
             showNavBar();
         }
         Fragment frag = mNavStacks[id].peek();
@@ -362,9 +362,7 @@ public class NavigationActivity extends BaseActivity
             return;
         }
 
-        if(!bdonly) {
-            showNavBar();
-        }
+
         boolean calcVisible = (mCalculatorView.getVisibility() == View.VISIBLE);
 
         hideCalculator();
@@ -376,6 +374,13 @@ public class NavigationActivity extends BaseActivity
                 homeIntent.addCategory(Intent.CATEGORY_HOME);
                 homeIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
                 startActivity(homeIntent);
+            }
+        } else {
+            if (fragment instanceof RequestQRCodeFragment) {
+                popFragment();
+                showNavBar();
+            } else {//needed or show nav before switching fragments
+                popFragment();
             }
         }
     }
