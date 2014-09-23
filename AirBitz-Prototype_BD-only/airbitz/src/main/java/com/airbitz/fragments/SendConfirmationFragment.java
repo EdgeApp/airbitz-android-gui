744bf4c308cf3deebe8dbd6b2fd839e627563860
tom <tom@airbitz.co>
2014-09-23 09:08:09 -0700

    fix crash if fragment not yet attached to activity
    Former-commit-id: a925aeb0416ab5ee9519cef2989039d6dc912e89

--- AirBitz-Prototype_BD-only/airbitz/src/main/java/com/airbitz/fragments/SendConfirmationFragment.java
+++ AirBitz-Prototype_BD-only/airbitz/src/main/java/com/airbitz/fragments/SendConfirmationFragment.java
@@ -633,6 +633,7 @@ public class SendConfirmationFragment extends Fragment {
                 Common.LogD(TAG, "Error during send "+txResult.getError());
                 if(mActivity!=null) {
                     mActivity.popFragment(); // stop the sending screen
+                    mActivity.getFragmentManager().executePendingTransactions();
                     mActivity.ShowOkMessageDialog(getResources().getString(R.string.fragment_send_confirmation_send_error_title), txResult.getError());
                 }
             } else {
