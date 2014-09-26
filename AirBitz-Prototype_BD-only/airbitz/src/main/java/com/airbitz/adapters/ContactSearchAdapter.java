0417c3cf818063ad3808510d3d347472175dfea5
tom <tom@airbitz.co>
2014-09-26 12:31:00 -0700

    contact picker working

--- AirBitz-Prototype_BD-only/airbitz/src/main/java/com/airbitz/adapters/ContactSearchAdapter.java
+++ AirBitz-Prototype_BD-only/airbitz/src/main/java/com/airbitz/adapters/ContactSearchAdapter.java
@@ -51,32 +51,39 @@ public class ContactSearchAdapter extends ArrayAdapter {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        ViewHolderItem viewHolder;
-        if(convertView==null){
-            LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            convertView = inflater.inflate(R.layout.item_listview_transaction_detail_business, parent, false);
+//        ViewHolderItem viewHolder;
+//        if(convertView==null){
+//            LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+//            convertView = inflater.inflate(R.layout.item_listview_transaction_detail_business, parent, false);
+//
+//            viewHolder = new ViewHolderItem();
+//            viewHolder.textView = (TextView) convertView.findViewById(R.id.transaction_detail_item_name);
+//            viewHolder.textView.setTypeface(BusinessDirectoryFragment.montserratRegularTypeFace);
+//            viewHolder.imageView = (ImageView) convertView.findViewById(R.id.transaction_detail_item_imageview);
+//            viewHolder.infoView = (TextView) convertView.findViewById(R.id.transaction_detail_item_address);
+//
+//            convertView.setTag(viewHolder);
+//        }else{
+//            viewHolder = (ViewHolderItem) convertView.getTag();
+//        }
 
-            viewHolder = new ViewHolderItem();
-            viewHolder.textView = (TextView) convertView.findViewById(R.id.transaction_detail_item_name);
-            viewHolder.textView.setTypeface(BusinessDirectoryFragment.montserratRegularTypeFace);
-            viewHolder.imageView = (ImageView) convertView.findViewById(R.id.transaction_detail_item_imageview);
-            viewHolder.infoView = (TextView) convertView.findViewById(R.id.transaction_detail_item_address);
-
-            convertView.setTag(viewHolder);
-        }else{
-            viewHolder = (ViewHolderItem) convertView.getTag();
-        }
+        LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        convertView = inflater.inflate(R.layout.item_listview_transaction_detail_business, parent, false);
+        TextView textView = (TextView) convertView.findViewById(R.id.transaction_detail_item_name);
+        textView.setTypeface(BusinessDirectoryFragment.montserratRegularTypeFace);
+        ImageView imageView = (ImageView) convertView.findViewById(R.id.transaction_detail_item_imageview);
+        TextView infoView = (TextView) convertView.findViewById(R.id.transaction_detail_item_address);
 
         Contact contact = mContacts.get(position);
-        viewHolder.textView.setText(contact.getName());
+        textView.setText(contact.getName());
         if(contact.getEmail()!=null) {
-            viewHolder.infoView.setText(contact.getEmail());
+            infoView.setText(contact.getEmail());
         } else if (contact.getPhone()!=null) {
-            viewHolder.infoView.setText(contact.getPhone());
+            infoView.setText(contact.getPhone());
         }
 
-        if(contact.getThumbnail()!=null)
-            picasso.load(Uri.parse(mContacts.get(position).getThumbnail())).noFade().into(viewHolder.imageView);
+        if(contact.getName()!=null && contact.getThumbnail()!=null)
+            picasso.load(Uri.parse(contact.getThumbnail())).noFade().into(imageView); //mPhotos.get(contact.getName())).noFade().into(imageView);
         return convertView;
     }
 }
