e542db22a8b9aa80df56760de525e49aa726a0cf
tom <tom@airbitz.co>
2014-10-28 11:24:35 -0700

    updates to image view pager, proper first touch, zoom, retention per image

--- AirBitz/airbitz/src/main/java/com/airbitz/fragments/ViewPagerFragment.java
+++ AirBitz/airbitz/src/main/java/com/airbitz/fragments/ViewPagerFragment.java
@@ -3,6 +3,9 @@ package com.airbitz.fragments;
 import android.app.Fragment;
 import android.graphics.Matrix;
 import android.graphics.PointF;
+import android.graphics.Rect;
+import android.graphics.RectF;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.util.FloatMath;
@@ -23,28 +26,13 @@ import java.util.List;
 /**
  * Created on 10/27/14.
  */
-public class ViewPagerFragment extends Fragment implements View.OnTouchListener {
+public class ViewPagerFragment extends Fragment {
     final String TAG = getClass().getSimpleName();
 
     private HighlightOnPressImageButton mQuitButton;
     private List<ImageView> mImageViews = new ArrayList<ImageView>();
     private ViewPager mViewPager;
     private int mPosition;
-
-// These matrices will be used to move and zoom image
-    Matrix matrix = new Matrix();
-    Matrix savedMatrix = new Matrix();
-
-// We can be in one of these 3 states
-    static final int NONE = 0;
-    static final int DRAG = 1;
-    static final int ZOOM = 2;
-    int mode = NONE;
-
-// Remember some things for zooming
-    PointF startPoint = new PointF();
-    PointF midPoint = new PointF();
-    float oldDist = 1f;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -68,7 +56,7 @@ public class ViewPagerFragment extends Fragment implements View.OnTouchListener 
         mViewPager.setAdapter(new ImageViewPagerAdapter(mImageViews));
         mViewPager.setCurrentItem(mPosition);
         for(ImageView iv : mImageViews) {
-            iv.setOnTouchListener(this);
+            iv.setOnTouchListener(new ImageTouchListener());
         }
 
         return mView;
@@ -79,61 +67,88 @@ public class ViewPagerFragment extends Fragment implements View.OnTouchListener 
         mPosition = position;
     }
 
-    public boolean onTouch(View v, MotionEvent event) {
-        ImageView view = (ImageView) v;
-        view.setScaleType(ImageView.ScaleType.MATRIX);
-        float scale;
+    private class ImageTouchListener implements View.OnTouchListener {
+        // These matrices will be used to move and zoom image
+        Matrix matrix = new Matrix();
+        Matrix savedMatrix = new Matrix();
 
-        // Handle touch events here...
-        switch (event.getAction() & MotionEvent.ACTION_MASK) {
+        // Touches can be in one of these 3 states
+        final int NONE = 0;
+        final int DRAG = 1;
+        final int ZOOM = 2;
+        int mode = NONE;
 
-            case MotionEvent.ACTION_DOWN: //first finger down only
-                savedMatrix.set(matrix);
-                startPoint.set(event.getX(), event.getY());
-                mode = DRAG;
-                Log.d(TAG, "action down");
-                break;
+        // Remember some things for zooming
+        PointF startPoint = new PointF();
+        PointF midPoint = new PointF();
+        float oldDist = 1f;
+        boolean firstTouch = true;
 
-            case MotionEvent.ACTION_UP: //first finger lifted
+        @Override
+        public boolean onTouch(View v, MotionEvent event)
+        {
+            ImageView view = (ImageView) v;
+            view.setScaleType(ImageView.ScaleType.MATRIX);
+            float scale;
 
-            case MotionEvent.ACTION_POINTER_UP: //second finger lifted
-                mode = NONE;
-                Log.d(TAG, "action up");
-                break;
+            // Handle touch events here...
+            switch (event.getAction() & MotionEvent.ACTION_MASK) {
 
-            case MotionEvent.ACTION_POINTER_DOWN: //second finger down
-                oldDist = spacing(event);
-                if (oldDist > 5f) {
-                    savedMatrix.set(matrix);
-                    midPoint(midPoint, event);
-                    mode = ZOOM;
-                }
-                Log.d(TAG, "action pointer up");
-                break;
-
-            case MotionEvent.ACTION_MOVE:
-                if (mode == DRAG) { //movement of first finger
-                    matrix.set(savedMatrix);
-                    matrix.postTranslate(event.getX() - startPoint.x, event.getY() - startPoint.y);
-                }
-                else if (mode == ZOOM) { //pinch zooming
-                    float newDist = spacing(event);
-                    if (newDist > 5f) {
-                        matrix.set(savedMatrix);
-                        scale = newDist / oldDist;
-                        matrix.postScale(scale, scale, midPoint.x, midPoint.y);
+                case MotionEvent.ACTION_DOWN: //first finger down only
+                    if(firstTouch) { // setup initial matrix
+                        RectF drawableRectF = new RectF(0, 0, view.getDrawable().getBounds().width(), view.getDrawable().getBounds().height());
+                        RectF viewRectF = new RectF(0, 0, view.getWidth(), view.getHeight());
+                        matrix.setRectToRect(drawableRectF, viewRectF, Matrix.ScaleToFit.CENTER);
+                        Log.d(TAG, "first touch");
+                        firstTouch = false;
                     }
-                }
-                Log.d(TAG, "action move");
-                break;
+                    savedMatrix.set(matrix);
+                    startPoint.set(event.getX(), event.getY());
+                    mode = DRAG;
+                    Log.d(TAG, "action down");
+                    break;
 
+                case MotionEvent.ACTION_UP: //first finger lifted
+
+                case MotionEvent.ACTION_POINTER_UP: //second finger lifted
+                    mode = NONE;
+                    Log.d(TAG, "action up");
+                    break;
+
+                case MotionEvent.ACTION_POINTER_DOWN: //second finger down
+                    oldDist = spacing(event);
+                    if (oldDist > 5f) {
+                        savedMatrix.set(matrix);
+                        midPoint(midPoint, event);
+                        mode = ZOOM;
+                    }
+                    Log.d(TAG, "action pointer up");
+                    break;
+
+                case MotionEvent.ACTION_MOVE:
+                    if (mode == DRAG) { //movement of first finger
+                        matrix.set(savedMatrix);
+                        matrix.postTranslate(event.getX() - startPoint.x, event.getY() - startPoint.y);
+                    }
+                    else if (mode == ZOOM) { //pinch zooming
+                        float newDist = spacing(event);
+                        if (newDist > 5f) {
+                            matrix.set(savedMatrix);
+                            scale = newDist / oldDist;
+                            matrix.postScale(scale, scale, midPoint.x, midPoint.y);
+                        }
+                    }
+                    Log.d(TAG, "action move");
+                    break;
+
+            }
+
+            // Perform the transformation
+            view.setImageMatrix(matrix);
+
+            return true; // indicate event was handled
         }
-
-        // Perform the transformation
-        view.setImageMatrix(matrix);
-
-        return true; // indicate event was handled
-    }
+    };
 
     private float spacing(MotionEvent event) {
         float x = event.getX(0) - event.getX(1);
