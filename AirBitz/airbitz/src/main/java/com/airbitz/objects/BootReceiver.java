339c4b5ca70ad4a87d1877bdb53476cddbaedddc
tom <tom@airbitz.co>
2014-11-24 15:31:04 -0800

    start notification alerts on system boot

---
+++ AirBitz/airbitz/src/main/java/com/airbitz/objects/BootReceiver.java
@@ -0,0 +1,68 @@
+/**
+ * Copyright (c) 2014, Airbitz Inc
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms are permitted provided that
+ * the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 3. Redistribution or use of modified source code requires the express written
+ *    permission of Airbitz Inc.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * The views and conclusions contained in the software and documentation are those
+ * of the authors and should not be interpreted as representing official policies,
+ * either expressed or implied, of the Airbitz Project.
+ */
+
+package com.airbitz.objects;
+
+import android.app.AlarmManager;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.os.AsyncTask;
+import android.os.Handler;
+import android.os.PowerManager;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
+
+import com.airbitz.AirbitzApplication;
+import com.airbitz.R;
+import com.airbitz.activities.NavigationActivity;
+import com.airbitz.api.AirbitzAPI;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/*
+ * Start Airbitz specific code on boot
+ */
+public class BootReceiver extends BroadcastReceiver {
+    final private String TAG = getClass().getSimpleName();
+
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        AirbitzAlertReceiver.SetRepeatingAlertAlarm(context);
+    }
+}
