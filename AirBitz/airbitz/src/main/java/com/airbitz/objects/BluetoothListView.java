2c94528df92e1e0924465f0b11bc99c1c14eeb64
Thomas Baker <baker.tom.w@gmail.com>
2015-01-10 11:12:41 -0800

    add protocol detailed description

--- AirBitz/airbitz/src/main/java/com/airbitz/objects/BluetoothListView.java
+++ AirBitz/airbitz/src/main/java/com/airbitz/objects/BluetoothListView.java
@@ -65,15 +65,67 @@ import java.util.List;
 import java.util.UUID;
 
 /*
- * Airbitz protocol
- *
- * Peripheral     Central   (Peripheral is the advertiser, this is Central)
- *    |------------->|     Advertises partial bitcoin: request + Name
- *    |     ...      |
- *    |<-------------|     Subscribe then send Name, requesting a response
- *    |------------->|     ACK
- *    |<-------------|     Read Characteristic
- *    |------------->|     Respond with full bitcoin: request
+ BitBeacon - an Airbitz BLE transfer protocol for a bitcoin request
+
+ Requestor      Sender    Network  - Bitcoin transaction roles
+ Peripheral     Central            - Bluetooth GAP definitions
+   Mode           Mode
+    |--BitBeacon-->|        |      - Requestor Advertises partial bitcoin: URI + Name
+    |     ...      |        |
+    |<-------------|        |      - Subscribe then send sender's Name, requesting a response
+    |------------->|        |      - ACK
+    |<-------------|        |      - request Read Characteristic from peripheral
+    |------------->|        |      - Sender receives full bitcoin: URI
+    |     ...      |        |
+    |              |------->|      - Sender initiates bitcoin transaction to network
+    |<----------------------|      - Requestor receives transaction confirmation from network
+
+ The BitBeacon protocol over Bluetooth Low Energy (BLE) uses available BLE protocols to create
+ a useful service in transferring a payment request to a sender over a local BLE unsecured
+ connection. Only a public URI is transferred just like a text message or email, maintaining
+ normal bitcoin security.
+ Instead of the requestor needing to present a POS terminal's QR code to the sender,
+ Bluetooth Low Energy broadcasts the request. The sender sees this
+ broadcast if their device is capable and configured appropriately.
+ The sender would select the proper request, in case of other simultaneous BitBeacon transactions,
+ then receive the full bitcoin URI request, the same as from any other source.
+ This is the end of the BitBeacon protocol.
+
+ The sender then completes the bitcoin transaction by starting a transfer of bitcoin
+ on the bitcoin network. The requestor waits to receive confirmation of bitcoin from the bitcoin
+ network. When that occurs, the bitcoin transaction is complete.
+
+ Protocol technical details
+ The Peripheral (requester) is in active scan mode to send a 62 byte advertisement. This
+ advertisement has 3 Ad Structures of which 2 are of primary interest for the BitBeacon transfer:
+ Type 7: 128 bit UUID - BitBeacon UUID identity
+ Type 9: Local Name - Partial Public Address and optional Identity characters
+
+ The Local Name is limited to 29 for iOS devices, and 26 for Android devices as of
+ Android 5.0.1. Local Name's first 10 characters are the first 10 characters of the public address in
+ the bitcoin URI. The rest of the Local Name is whatever Identity characters are needed for the
+ sender to know it is from the requestor specifically, i.e. "Joe's Grill 2".
+
+ Sender's name is also limited. For Android it's 18 characters. TBD iOS.
+
+ Definitions
+ BLE - Bluetooth Low Energy
+ Peripheral - Bluetooth SIG definition of the advertiser / beacon
+ Central - Bluetooth SIG definition of the scanner device listening for advertisements
+ Requester - A bitcoin user making a transfer request to another user
+ Sender - A bitcoin user sending bitcoin to a requester
+ Network - the distributed bitcoin transaction network
+*/
+
+/* BluetoothListView specific
+ The Central device is put into active scan mode to request the additional Local Name type
+ which contains the first 10 bytes of the URI public address and optionally a text identifier
+ up to the limits of the advertisement data. This is for the Sender
+ to view in this list to visually verify the Requestor in case of multiple, simultaneous
+ BLE transactions.
+ Once a list item is clicked, the Sender receives the full bitcoin: URI with full
+ public address and optionally the amount and requester's payment name. This request is
+ announced back to this list's observer.
  */
 
 @TargetApi(21)
