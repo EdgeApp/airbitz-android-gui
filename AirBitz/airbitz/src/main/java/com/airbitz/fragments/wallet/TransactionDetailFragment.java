5c7997b44aba980e118612441ce8a98ebb090c65
Thomas Baker <baker.tom.w@gmail.com>
2015-03-17 13:12:48 -0700

    fix crash on send just now

--- AirBitz/airbitz/src/main/java/com/airbitz/fragments/wallet/TransactionDetailFragment.java
+++ AirBitz/airbitz/src/main/java/com/airbitz/fragments/wallet/TransactionDetailFragment.java
@@ -840,8 +840,8 @@ public class TransactionDetailFragment extends BaseFragment
             mPayeeImageViewFrame.setVisibility(View.VISIBLE);
 
             mPayeeImageView.setLayerType(View.LAYER_TYPE_SOFTWARE, null);
-            int round = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 20, getResources().getDisplayMetrics());
-            int dimen = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 160, getResources().getDisplayMetrics());
+            int round = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 20, mActivity.getResources().getDisplayMetrics());
+            int dimen = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 160, mActivity.getResources().getDisplayMetrics());
             if (payeeImage.getScheme().contains("content")) {
                 try {
                     Bitmap bitmap = MediaStore.Images.Media.getBitmap(mActivity.getContentResolver(), payeeImage);
