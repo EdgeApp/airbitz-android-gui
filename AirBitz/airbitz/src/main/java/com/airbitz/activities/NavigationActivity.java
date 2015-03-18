3abfa1c03b016478272fad50f68437f0dd8958b5
Thomas Baker <baker.tom.w@gmail.com>
2015-03-18 13:33:10 -0700

    cleanup comment

--- AirBitz/airbitz/src/main/java/com/airbitz/activities/NavigationActivity.java
+++ AirBitz/airbitz/src/main/java/com/airbitz/activities/NavigationActivity.java
@@ -483,8 +483,6 @@ public class NavigationActivity extends Activity
             }
             transaction.replace(R.id.activityLayout, fragment);
             transaction.commitAllowingStateLoss();
-
-//            getFragmentManager().executePendingTransactions();
         }
     }
 
