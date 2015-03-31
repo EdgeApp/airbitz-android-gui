82c5ef144856aa85acedfb914881c76d53c0a384
Thomas Baker <baker.tom.w@gmail.com>
2015-03-30 20:53:01 -0700

    attempted fix "GPS issue"

--- AirBitz/airbitz/src/main/java/com/airbitz/objects/PlayLocationManager.java
+++ AirBitz/airbitz/src/main/java/com/airbitz/objects/PlayLocationManager.java
@@ -98,7 +98,9 @@ public class PlayLocationManager implements
 
     public Location getLocation() {
         if (null == mCurrentLocation && locationClient != null && locationClient.isConnected()) {
-            LocationServices.FusedLocationApi.getLastLocation(locationClient);
+            mCurrentLocation = LocationServices.FusedLocationApi.getLastLocation(locationClient);
+            Log.d(TAG, "getloc: CUR LOC, accuracy(m): " + mCurrentLocation.getLatitude() + "; "
+                    + mCurrentLocation.getLongitude() + ", " + mCurrentLocation.getAccuracy());
         }
         return mCurrentLocation;
     }
@@ -126,6 +128,8 @@ public class PlayLocationManager implements
         if(locationClient.isConnected()) {
             LocationServices.FusedLocationApi.requestLocationUpdates(locationClient, mLocationRequest, this);
             mCurrentLocation = LocationServices.FusedLocationApi.getLastLocation(locationClient);
+            Log.d(TAG, "onconnected: CUR LOC, accuracy(m): " + mCurrentLocation.getLatitude() + "; "
+                    + mCurrentLocation.getLongitude() + ", " + mCurrentLocation.getAccuracy());
         }
 
         if (mCurrentLocation != null) {
@@ -144,8 +148,8 @@ public class PlayLocationManager implements
         Log.d(TAG, "onLocationChanged");
         if (location.hasAccuracy() && !mObservers.isEmpty()) {
             mCurrentLocation = location;
-            Log.d(TAG, "CUR LOC: " + mCurrentLocation.getLatitude() + "; "
-                                   + mCurrentLocation.getLongitude());
+            Log.d(TAG, "onlocationchange: CUR LOC, accuracy(m): " + mCurrentLocation.getLatitude() + "; "
+                + mCurrentLocation.getLongitude() + ", " + location.getAccuracy());
 
             Iterator<CurrentLocationManager.OnCurrentLocationChange> i = mObservers.iterator();
             while (i.hasNext()) {
