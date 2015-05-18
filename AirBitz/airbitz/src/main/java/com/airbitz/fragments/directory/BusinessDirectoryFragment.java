00440d8f458123b9785a72919cdf6fe6182a5aa7
Tim Horton <tmhorton@gmail.com>
2015-05-18 11:00:39 -0700

    Bug fixes to support business directory language

--- AirBitz/airbitz/src/main/java/com/airbitz/fragments/directory/BusinessDirectoryFragment.java
+++ AirBitz/airbitz/src/main/java/com/airbitz/fragments/directory/BusinessDirectoryFragment.java
@@ -1,18 +1,18 @@
 /**
  * Copyright (c) 2014, Airbitz Inc
  * All rights reserved.
- * 
- * Redistribution and use in source and binary forms are permitted provided that 
+ *
+ * Redistribution and use in source and binary forms are permitted provided that
  * the following conditions are met:
- * 
+ *
  * 1. Redistributions of source code must retain the above copyright notice, this
- *    list of conditions and the following disclaimer. 
+ *    list of conditions and the following disclaimer.
  * 2. Redistributions in binary form must reproduce the above copyright notice,
  *    this list of conditions and the following disclaimer in the documentation
  *    and/or other materials provided with the distribution.
  * 3. Redistribution or use of modified source code requires the express written
  *    permission of Airbitz Inc.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -23,9 +23,9 @@
  * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- * 
+ *
  * The views and conclusions contained in the software and documentation are those
- * of the authors and should not be interpreted as representing official policies, 
+ * of the authors and should not be interpreted as representing official policies,
  * either expressed or implied, of the Airbitz Project.
  */
 
@@ -99,10 +99,6 @@ public class BusinessDirectoryFragment extends BaseFragment implements
     static int CATEGORY_TIMEOUT = 15000;
     static int LOCATION_TIMEOUT = 10000;
     static float LOCATION_ACCURACY_METERS = 100.0f;
-
-    static String SEARCH_RESTAURANTS = "Restaurants & Food Trucks";
-    static String SEARCH_BARS = "Bars & Nightclubs";
-    static String SEARCH_COFFEE = "Coffee & Tea";
 
     public static final String LOCATION = "LOCATION";
     public static final String BUSINESS = "BUSINESS";
@@ -304,7 +300,7 @@ public class BusinessDirectoryFragment extends BaseFragment implements
             @Override
             public void onClick(View view) {
                 Bundle bundle = new Bundle();
-                bundle.putString(BUSINESS, SEARCH_RESTAURANTS);
+                bundle.putString(BUSINESS, mRestaurantButton.getText().toString());
                 bundle.putString(LOCATION, "");
                 bundle.putString(BUSINESSTYPE, "category");
                 Fragment fragment = new MapBusinessDirectoryFragment();
@@ -317,7 +313,7 @@ public class BusinessDirectoryFragment extends BaseFragment implements
             @Override
             public void onClick(View view) {
                 Bundle bundle = new Bundle();
-                bundle.putString(BUSINESS, SEARCH_BARS);
+                bundle.putString(BUSINESS, mBarButton.getText().toString());
                 bundle.putString(LOCATION, "");
                 bundle.putString(BUSINESSTYPE, "category");
                 Fragment fragment = new MapBusinessDirectoryFragment();
@@ -330,7 +326,7 @@ public class BusinessDirectoryFragment extends BaseFragment implements
             @Override
             public void onClick(View view) {
                 Bundle bundle = new Bundle();
-                bundle.putString(BUSINESS, SEARCH_COFFEE);
+                bundle.putString(BUSINESS, mCoffeeButton.getText().toString());
                 bundle.putString(LOCATION, "");
                 bundle.putString(BUSINESSTYPE, "category");
                 Fragment fragment = new MapBusinessDirectoryFragment();
