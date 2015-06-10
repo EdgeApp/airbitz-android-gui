2332aa1898b2461fbcfe810eb43dffc747dbd73e
Tim Horton <tim@airbitz.co>
2015-06-10 12:14:55 -0700

    Added min length text field

---
+++ AirBitz/airbitz/src/main/java/com/airbitz/objects/MinEditText.java
@@ -0,0 +1,62 @@
+/**
+ * Copyright (c) 2014, Airbitz Inc
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms are permitted provided that
+ * the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 3. Redistribution or use of modified source code requires the express written
+ *    permission of Airbitz Inc.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * The views and conclusions contained in the software and documentation are those
+ * of the authors and should not be interpreted as representing official policies,
+ * either expressed or implied, of the Airbitz Project.
+ */
+
+package com.airbitz.objects;
+
+import android.content.Context;
+import android.text.InputFilter;
+import android.text.Spanned;
+import android.util.AttributeSet;
+import android.widget.EditText;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class MinEditText extends LimitedEditText {
+
+    private int mMinLength;
+
+    public MinEditText(Context context) {
+        super(context);
+    }
+
+    public MinEditText(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public MinEditText(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    public void setMinLength(int min) {
+        mMinLength = min;
+    }
+}
