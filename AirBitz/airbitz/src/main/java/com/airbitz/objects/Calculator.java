8f6d2de9956df6626c2337ccb1fc3c8eb8bb35bd
Tim Horton <tim@airbitz.co>
2015-06-24 23:15:58 -0700

    Updated styles for request fragment

--- AirBitz/airbitz/src/main/java/com/airbitz/objects/Calculator.java
+++ AirBitz/airbitz/src/main/java/com/airbitz/objects/Calculator.java
@@ -1,18 +1,18 @@
 /**
  * Copyright (c) 2014, Airbitz Inc
  * All rights reserved.
- * 
- * Redistribution and use in source and binary forms are permitted provided that 
+ *
+ * Redistribution and use in source and binary forms are permitted provided that
  * the following conditions are met:
- * 
+ *
  * 1. Redistributions of source code must retain the above copyright notice, this
- *    list of conditions and the following disclaimer. 
+ *    list of conditions and the following disclaimer.
  * 2. Redistributions in binary form must reproduce the above copyright notice,
  *    this list of conditions and the following disclaimer in the documentation
  *    and/or other materials provided with the distribution.
  * 3. Redistribution or use of modified source code requires the express written
  *    permission of Airbitz Inc.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -23,15 +23,16 @@
  * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- * 
+ *
  * The views and conclusions contained in the software and documentation are those
- * of the authors and should not be interpreted as representing official policies, 
+ * of the authors and should not be interpreted as representing official policies,
  * either expressed or implied, of the Airbitz Project.
  */
 
 package com.airbitz.objects;
 
 import android.content.Context;
+import android.os.Handler;
 import android.text.Editable;
 import android.util.AttributeSet;
 import android.view.KeyEvent;
@@ -39,6 +40,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.util.Log;
 
 import com.airbitz.R;
 
@@ -127,6 +129,8 @@ public class Calculator extends LinearLayout  {
         mView.findViewById(R.id.imageButtonBack).setOnClickListener(mOnKeyListener);
         mView.findViewById(R.id.imageButtonEquals).setOnClickListener(mOnKeyListener);
     }
+
+    private final String TAG = getClass().getSimpleName();
 
     // This is where the text enters and the results return
     public void setEditText(EditText editText) {
