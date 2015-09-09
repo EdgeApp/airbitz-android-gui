553cc2e7eee63f51c8b53a0bc016d0139cc6cb9d
William Swanson <swansontec@gmail.com>
2015-09-09 15:09:30 -0700

    Include the core using proper Android.mk technique
    The `LOCAL_SHARED_LIBRARIES` variable is the proper way to do this.
    That way, the build system can automatically copy things to the right places.

--- AirBitz/airbitz/src/main/jni/Android.mk
+++ AirBitz/airbitz/src/main/jni/Android.mk
@@ -1,14 +1,15 @@
 LOCAL_PATH := $(call my-dir)
-include $(CLEAR_VARS)
 
+# Tell the build system about libabc.a:
+include $(CLEAR_VARS)
+LOCAL_MODULE := airbitz-core
+LOCAL_SRC_FILES := $(TARGET_ARCH_ABI)/libabc.so
+include $(PREBUILT_SHARED_LIBRARY)
+
+# Build the JNI wrapper:
+include $(CLEAR_VARS)
 LOCAL_MODULE := airbitz
 LOCAL_SRC_FILES := ABC_wrap.c ABC_android_util.c
-LOCAL_LDLIBS  := -llog -labc
-
-ifeq ($(TARGET_ARCH_ABI),x86)
-	LOCAL_LDFLAGS := -L../jniLibs/$(TARGET_ARCH)/
-else
-	LOCAL_LDFLAGS := -L../jniLibs/armeabi/
-endif
-
+LOCAL_SHARED_LIBRARIES := airbitz-core
+LOCAL_LDLIBS  := -llog
 include $(BUILD_SHARED_LIBRARY)
