24a361f5c24d96d32e0008ef7b315c881efa1e6b
Tim Horton <tim@airbitz.co>
2016-06-16 17:34:44 -0700

    Ensure single semi-colons because bad translaction

--- Airbitz/airbitz/src/main/java/com/airbitz/utils/Common.java
+++ Airbitz/airbitz/src/main/java/com/airbitz/utils/Common.java
@@ -589,7 +589,7 @@ public class Common {
             prefix = translateBaseCategory(context, ""); // use the default
             suffix = cat;
         }
-        return prefix + ":" + suffix;
+        return (prefix + ":" + suffix).replace("::", ":");
     }
 
 }
