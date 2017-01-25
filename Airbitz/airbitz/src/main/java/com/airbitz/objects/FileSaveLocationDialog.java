b8e7aaf5402b29164a786d0d11a1668b74954edc
Kevin Sullivan <kevin@airbitz.co>
2017-01-24 16:37:04 -0800

    Fix SD card export directory selection bug
    When selecting a directory other than the default, the app crashed.
    The dialog adapter needed an onClickListener and onClick method.

--- Airbitz/airbitz/src/main/java/com/airbitz/objects/FileSaveLocationDialog.java
+++ Airbitz/airbitz/src/main/java/com/airbitz/objects/FileSaveLocationDialog.java
@@ -70,39 +70,37 @@ public class FileSaveLocationDialog implements AdapterView.OnItemClickListener {
         mFileList = getDirectoryListing(directory);
         mAdapter = new DirectoryListingAdapter(context, android.R.layout.simple_list_item_1, mFileList);
 
-        AlertDialogWrapper.Builder builder = new AlertDialogWrapper.Builder( new ContextThemeWrapper(mContext, R.style.AlertDialogCustom) );
-        builder.setTitle( directory.getAbsolutePath() );
-        builder.setAdapter( mAdapter, null );
+        AlertDialogWrapper.Builder builder = new AlertDialogWrapper.Builder( new ContextThemeWrapper(mContext, R.style.AlertDialogCustom) )
+                .setTitle( directory.getAbsolutePath() )
+                .setAdapter( mAdapter, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        if(id >= 0 && id < mFileList.size()) {
+                            mCurrentDirectory =
+                                mFileList.get(id).getName().equals("..")
+                                ? mCurrentDirectory.getParentFile()
+                                : mFileList.get(id);
 
-        builder.setPositiveButton(R.string.string_save, new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dialog, int id) {
-                if (mCallback != null )
-                    mCallback.onFileSaveLocation(mCurrentDirectory);
-                dialog.dismiss();
-            }
-        });
+                            mFileList = getDirectoryListing(mCurrentDirectory);
+                            mAdapter.notifyDataSetChanged();
+                            mAlertDialog.setTitle(mCurrentDirectory.getAbsolutePath());
+                        }
 
-        builder.setNegativeButton(R.string.string_cancel, new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dialog, int id) {
-                dialog.dismiss();
-            }
-        });
+                    }
+                } )
+                .setPositiveButton(R.string.string_save, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        if (mCallback != null ) mCallback.onFileSaveLocation(mCurrentDirectory);
+                        dialog.dismiss();
+                    }
+                })
+                .setNegativeButton(R.string.string_cancel, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.dismiss();
+                    }
+                });
 
         mAlertDialog = builder.show();
-//        mAlertDialog.getListView().setOnItemClickListener(this);
         mAlertDialog.show();
-    }
-
-    @Override
-    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-        if(i >= 0 && i < mFileList.size()) {
-            mCurrentDirectory = mFileList.get(i).getName().equals("..") ?
-                mCurrentDirectory.getParentFile() : mFileList.get(i);
-
-            mFileList = getDirectoryListing(mCurrentDirectory);
-            mAdapter.notifyDataSetChanged();
-            mAlertDialog.setTitle(mCurrentDirectory.getAbsolutePath());
-        }
     }
 
     private List<File> getDirectoryListing(File directory) {
@@ -112,6 +110,7 @@ public class FileSaveLocationDialog implements AdapterView.OnItemClickListener {
         else {
             mFileList.clear();
         }
+
         if(directory.getParent() != null) {
             mFileList.add(new File(".."));
         }
